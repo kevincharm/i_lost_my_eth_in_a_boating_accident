24a51353863852f906679cf89e196321ebb678a6
jjvalkema <jjvalkema@protonmail.com>
2024-05-09 00:51:16 +0200

    fixed noirjs not taking inputs. added input field for the proof since wasm runs out of memory

--- website/main.js
+++ website/main.js
@@ -1,17 +1,15 @@
 import  {BarretenbergBackend} from '@noir-lang/backend_barretenberg';
-import { Noir } from '@noir-lang/noir_js';
+import { Noir, blake2s256 } from '@noir-lang/noir_js';
+// window.Noir = Noir
+// window.blake2s256 = blake2s256
 import {assert, ethers, wordlists} from 'ethers'
 //import circuit from "../circuit/target/circuit.json"
 const circuit ={"noir_version":"TODO"}
 import { SunkEthInteracter } from './src/SunkEthInteracter';
+window.SunkEthInteracter = SunkEthInteracter
 //const sunkETHABI =["function decimals() view returns (uint8)","function symbol() view returns (string)","function balanceOf(address owner) view returns (uint256)","function transfer(address to, uint amount) returns (bool)"]
-import {processProof} from '../scripts/processProof'
-import { getStorageKey } from '../scripts/getStorageKey'
 
 import {toUnspendableAddress} from '../scripts/toUnspendableAddress'
-
-window.processProof = processProof
-window.getStorageKey = getStorageKey
 
 const sunkEthContractAddress = "0x46CFe55bf2E5A02B738f5BBdc1bDEE9Dd22b5d39"
 window.ethers = ethers
@@ -39,6 +37,8 @@ async function getSigner(provider) {
       return false
     }
   }
+
+
 
 
 async function connectWallet() {
@@ -91,11 +91,12 @@ async function showDeposits() {
   const SECRET_KEY = '0xbc005f65414869c630b7ae67836847f46f9083a9cd84af3f01969031309e5c1d'
   const burnAddress = toUnspendableAddress(SECRET_KEY)
   const BALANCE_TO_BURN = ethers.parseEther('0.069420')
-  const deposits = {[burnAddress]:{
-    amount: BALANCE_TO_BURN,
-    nullifierData: {secret: SECRET_KEY},
-    isClaimed: false
-  }}//await sunkEthInteracter.getDepositFromLocalStorage()
+  // const deposits = {[burnAddress]:{
+  //   amount: BALANCE_TO_BURN,
+  //   nullifierData: {secret: SECRET_KEY},
+  //   isClaimed: false
+  // }}
+  const deposits = await sunkEthInteracter.getDepositFromLocalStorage()
   
   for(const address in deposits) {
     const deposit = deposits[address]
@@ -165,7 +166,29 @@ async function connectWalletHandler() {
 
 async function unshieldHandler(nullifierData) {
   const {sunkEthInteracter} = await connectWallet()
-  const tx  = await sunkEthInteracter.unShieldTokens(nullifierData)
+  // const hardCodedSnarkProof = "0x"+"19c5e8a75a4eb3a315a60b17db6594bd222c336d93500ff52d07374a82f7d907085fd9f9cc18c5bfff6b8781cce50df208d481d48a07ab529bf958105546435009beb8a4aa1ea84c17f4a10d6ddc94c5034afd07979782f4be80353a9adcdbe22e96e883cc6bd829adafda6dd6dc9c917845d8fab8a1b78f7de77886cbc3d82e27c7155110879507713e46ce4f2e5defd6b1f2c5552f123d6356031b9c2dae3c0b9499c8b99174ca3f280ac16eefac9ca60cb3d32bb0181d9e41f0468397069f2c1c3c1a042c56a83e1aba8e9ac3e2b283ff1f7968394b973718df351237b003278c0f09cf4fd6a8ee21e24908a8fdd339a969595dcb2f7ee350b7487bce894925af3205a952fc18a60f35baf5057e2330cfb7e0e6966a42c11ea856b929897f09fe91114fbccf021f995354f10badaf178510d463f11e17ca55eecc5312e04108eeaa3195de1a0d1b2342c5efaf1511607c4ecaf6d20b5e934240ccd433a4ff2b72b927f2cf5e4fbe6c422d45eec2afc71035a4c99771c820bf2c179984d4ef07aa5840b2af6bc0a783c2ad861f2de3c98db73fc96d7a44247dc732001cb6a12da3f4d791a2c85022cfffbe5eb7ce88c9ee3e631a2efc432497f483536eb5030af238409c150b2e074e1fd37f01415124f1a8bb96b5fcadb0ed79ad421655be246b6cce980c217971dccafa70bbd67cf7d39cb8100bd7d246c605d74611fb3b1d901ff4f4d564ca95c59ac721cd334e538d7688d883c06fd31002aace2d9bee2aee1ef6586ccfeda909208482f46d7d43e48f181bf71cbe841b96d05594ca831291514caab7e2e763e5bc04b4bb8d902c788cc8e5b9999050f314e41fdf1b6e2e7754ee05bfc120bc44647411de7d33565663b90b517a26ea45782c1a5efd330dc18cd7694eed7dc07189ee7f88bd1cfec5b9ceff4960308839f6e6c8c2ae0a2d1b94871e0972ab4a871f633da2ba9d237ae1540d0f8d00bb9a96484822bb7002093fa39c9af01f3e49a18edd595189876a3ddee7ebebec7831cb34953707922883ff21592606cf7d7b8acd955520a53624214f4d8d5c5bca9c8f6e8def7df90df5c755978461e276a0de17f04a6b7d82a99058b30d964f27abf487a3a1b8862ed7ae13abc9bd95ad4e2a58b41a10d82f36b651e6937a31d601fb123c2131aa28db0856d5f0bad909094714b910800aaebd30e4b8ad3784ffc01b0e8ab4fb72239d87cc35c28743a60b3157fb8f61ff1301d00ab0992e6b59063b9bde2bfeaa091aa5228e01d3a7d62d7c41965072209509b53d9d88d745502215a707cd57dd22a26dd0609ab580b2ae277c5235d4e26fe3fa1797b72ebc36d3cba9a5cafa3d1fc305e674876509b86290c54cd47194e0b31f71e7b2ec0e4a1072d8810ec2ac1a50dcbe69852b1c733571721fd5fc41064fbe0e729fdaa4c98636b638590e73218d2e72079158c10f000c22a45087f731cda7d5a9c3267326293b02c09047dd24d3258694dfbf610cdb33226dc57b0241423bf63044d6f2d4f31564c64a91031fc7c819edb0df0dda31ad19c03c941957f95a0c1faf4305275a4da6098b400403e1744388d0db2761aec7df9e1be798a808c45d206e6352588005a75a2ba08e219d4e8ff01983fabf196287fc7f2d58955a9c8bc00715581c13e5e54a9e31281804bd2b165b0940ee3cb86ec04ea5967fd0d5a04e9246817b5dd5eec8bc2a1726c6638a90bcb1c109439806519a6df14184e5e143103793269dc95a5f7400e91a901876260765cebc8e29759f13a5910707982c78af7b6be32fc38aa13fda2f293b59a27209324c52201cd2b00e01e6f3f32e6e85369be2c251d847f8e062c10d7780ec149845cd9cefa641f9d43a90452551100d2dde6c28ff376f12f6c64718c964aadf0fe241fc6438a81d70d9fd3195bcf1bfeb92a65196155ae152654124af44a78e9f203efa078c9e6e18267931dc1dac0d6b2f3b78d9a7038ef500c1300a9344feddd8906895f741ec02144c9f65e98e971d3083c2c67cc7a66de83e0950093365058ff618b16a9dcb570e57c99ddd28375cd719a941addd5f75f7b81c8e01772b6366d5f51bbe045d7d4fe352283fe0629ee9e63e2ba45026addde62e5eeb4cf8feeee1314950e2650fb4e06f54dc98b7bc57218acc8c33361512aa2df25575ac41e977bdc141f191e88946e7d54be776d9506d72fe43265ed77477203e40c8e6960feeeeb1ca3093e817fa4464fe8e035b7c3f353dca9fbdb69ca1039fb4699096be77cf4f4b488c81590dfa11cf6b257f5d95692e0de20c3350af0e4553074de2fc688d6c52635443de68633c5f1e2ede43e6c3b5243f77d802bd054ea137d889c490638d7ad1d3a596713e78c378328f4478c5fbe20d5619619a0f8ba40c482aa654739490d485954bfda8d56d458e9797d9711d28d984e2e8e627903c689896b06a9ddf6cf42ccf23ae4ba3a1b35f2824d659e8f20f15c9934d1ffc5adaf87b54726c39402cd5f5db95f89d0933d89d405e903838c64cff7f45026515661cf6806b8a6ce57b9b53a8405c347e646d70902b3aa5e7ac66fa44b60b7ca04b95c51678d1115f22327b416ed4fab1059ae820e05c69fa5226bf783e03809475fd0015eb36be86294e47521856ac55b23ed633362ee3f17e4549ffec17abf7a8bf01f34fd97f112802a05c595f9ce4211c364c8fd159c0cb7cbe74ed1b3aaa4baa5b7220c72b4d53f807acf59cf19f6d1ab53cd951388f2dcb15109504151c48571a1ffabcb18c485177f95c91707ab9fb1aa7b24d791dbba317c02c22a915e8610b2820b0995c92f1eb7b82841cf82bb705aea0595e6a6e16fa19b326a75ffc3f455218e464340cf88714032ffd684faa1aec20f9794f49648ce3ea2527bd98dfb83ab71087504e3eb83f7b24673bb1383877d835953ddf3631dc5a00fe9a2d994ef749736dd196512fb80bbfe63556ce2560a47484ebfb72d27e601405c717dbf614aae1e10d8a704278ec9d79e9a73337c76db02bc2dda3e6c2cb"
+  // const secret = "0xf986d0f3842ff6a98a1ee59350fec002f4c5983413745711081bf3e5aadedb6e"
+  // const blockNumber = 5837118
+
+  
+  const provider = getBrowserProvider()
+  const blockNumber = (await provider.getBlock()).number
+  const secret = ethers.hexlify(nullifierData.secret)
+  const tx  = await sunkEthInteracter.unShieldTokens(secret,blockNumber)
+  console.log(await tx.wait(1))
+  await refreshUiValues()
+}
+
+async function unshieldHandlerExternalProofHandler() {
+  const {sunkEthInteracter} = await connectWallet()
+
+
+
+  const proof = "0x" +  document.getElementById("proof").value
+  const secret = document.getElementById("secret").value
+  const blockNumber = Number(document.getElementById("blockNumber").value)
+  console.log(secret,blockNumber, proof)
+  const tx  = await sunkEthInteracter.unShieldTokens(secret,blockNumber, proof)
   console.log(await tx.wait(1))
   await refreshUiValues()
 }
@@ -185,6 +208,9 @@ function setEventListeners() {
 
   const shieldBtn = document.getElementById("shieldBtn")
   shieldBtn.addEventListener("click",async ()=>await shieldTokenBtnHandler())
+
+  const remintExternalProofBtn = document.getElementById("remintExternalProof")
+  remintExternalProofBtn.addEventListener("click", async () => await unshieldHandlerExternalProofHandler())
 
 }
 
--- website/src/SunkEthInteracter.js
+++ website/src/SunkEthInteracter.js
@@ -5,13 +5,16 @@ import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
 import { Noir } from '@noir-lang/noir_js';
 // import {getStateProofs} from "./getStateProofs"
 import {getBlockWithRLP} from './rlp.js'
-
+import assert from 'assert';
 import { getStorageKey } from '../../scripts/getStorageKey'
+import {processProof} from '../../scripts/processProof'
+
 import { toUnspendableAddress } from '../../scripts/toUnspendableAddress'
 
 //TODO replace with the real circuit
 import circuit from "../../circuit/target/sunketh_circuit.json"
 // import { c } from 'vite/dist/node/types.d-aGj9QkWt';
+
 
 // const STORAGE_KEY = getStorageKey(0n, '0x97248C0ddC583537a824A7ad5Ee92D5f4525bcAa')
 export const MAX_DEPTH = 8
@@ -19,10 +22,21 @@ export const MAX_TRIE_NODE_LENGTH = 532
 export const MAX_ACCOUNT_STATE_LENGTH = 134
 export const MAX_STORAGE_VALUE_LENGTH = 32
 
-const snarkProof = "0x2aadde2ac1786695ab009761b6e953f77a008ee91c0dc0181bd9b9a5c5fb77580cbe9d218617fb5f5b30ceb780fb90d12bf12ca8359ffee96e4a96e8e50a8fb51c553e43946c82c1d342be6140fa5fe67cd30d5c60bdffa341910aba7d967e4901081a7e829cc865025b633e2bf4393a42f268309e155625a8373832ceb307bc2446e6a5034c33e6e52d784c786431e556069d1daf6ae15bb141910a6d596a161cb68446776d54c54337739121816f34c54a311e167f8f0579ff59af8d642cd228b8e6c7082c6be26277df19b1ed61e8db9bad37314c535c012e79f931294ac72e60db27c0051642b8bd29cf80b040dd179e71c84e5e9b7fce5c2ceccd23a98a095f0d1733dbec39d81077998c3f364a88326ad72e5cbf0098bd7e29fe2f1c0418e4865dda36e04a034b3da7eb9c80535a990d02228761cbe9ca4bbb2faa522a2321e913aae2739ddf60744c503f2c565e187cab7ba86e5710afafc186b32b9b28450698721219bf2dd54d75f5d142b043d95c77b1502a6b35274af3d26fdcc21e7debfef9a8e9efcee8d03fbbd565432c5497aef0e2354fe091b284e5d7faaa22c97f6763df55dd29a7695aaa808c7a05cb076cf77ea527c5aaf38bf44b242b0190afdf5e0c842e68ac40d1ceeb18d1a935b628b6a918a231778222d263bd002ad2719b3bb9e6c8d1d9543bd66850a055614f53966cecf051b1757d65ab91231156c077f18b4b25be0f33358ea6b12477acb631cb9b62ffd2636125790f6e3b060a2b86486477053a7d6178bff26e512aa32b2fc9f5c32a19dc3819784669642b0f164651e53202edd3333908809150a436bb31d5811ecc6fb5835209c3da290ac8f37735fbcd2f4b29a84b1d0a3dbddf7771f0b4b9c94bf9e7549138fcfd2f039a44f0c51fbb0d3c228b22a75a32b64fe382bb9555ca8d124fbc46ba8787c8067bf57fe60a8bef0032258aec6f7ee21965b0536da2acb498497d8f8b10bf4f08749c88b89814433d950fa44a703a59b9c333c10d2d8006b0e91a1127a78d9f0e964efaf454785e681ae71e42d7107c95de582ec0b164da89e5750ce17ff7e218b238fe91e3d506efa8bdd28ee6fe4487dcad3b20c5a3a1299f672a14bc4e960e9873ef07b9f8a2265461295ffcdd9a5a9fd45784782cc73b3db3e4fdfa9c0422b31c72a035805abad5389a7e04b4bf92b34a46e162dcedd630909b7c4106950d9bce988785e282b0aa1ff7cbd4f455b3118ee3ea569c4acbd9d775a74ecfa81e91f6f705c6b4277b28afad13e2970cc3c0f7a06317a643076398f0ac6768ac2a7d472630be405b2359c655e696001a5b2e4101de37416174b142295a7b9f1b12d118332f9ea2fdfd434af77ebdf494c341fc4628698329ff88d27cde58fdb128cac7ad23962d943e4564ec777ac30979a3069e1b9d5a3f2e6bb41490474d6a2701f9894cd54dc60d368a910177242abc7d88e3eba95fef7e45940ab71c4d1d06b2e7ee62eff311d43d2534930acd350c4e4ec77dc5445ea0df7d7cfe4b991f028a387668d4bf95148f37906ae39d934832257852ae3db8deac993c77c528142d21681b78727f105c819bd24d19b9d2cec45ef5c72b96c520fbf47044fb16dd2161c35fc10b12d1534e8de57e3213d3818b5d411962965ab26cce46880e53e015c9181ba6c00f959ca0f209be5d91f46c7b5ed56d2e5ccb14426a84e4d40a5e02f5c7d22297c61ca99e90afbb162fb0aeb463b30124b41b31a91fb0e29b7ea705000be39c8fe2e5bbb59bbe65831edded78446d734ae8bf11ad22223443ffc2093c7316b023a35dcaf41ef1e7762fb7755abc8b14995feb040fe50c85843ac22075fede8e9abea7a6b91f669a4bc1839713e3eb6e5316a3caafbf8c323229be129c36a771f9f95c63ddc944cd18a22f034195d8a5242db61bb658f3c244212f137c0b1eeb94340b6a75d4d6d6f856ccc416045971533e4e7b0a56c17cf0e5ff24838f119ba1833bca2ac10e5acfb2f943f0d253d97b3df1df1247f0a4b4abf3288d3078e9774e028c48ab0c8264ab248d35ee15a214ee63b275a4440c58e5662c7488eb7bfeff03113266561e0e4d6217e8b122c180c7680a409004fcc36a4d2f38cb452bb979ba16bf78b1da1195b2f53ef3a39154e204c93184dff967223c27938aedf76ddb2d8c7c0d232d08bd8f8a77176807f430a34be4c9dba5e4964c0d436415139b8eb218269ef2367ec02f0971f3cb0c51ce8900a54d34ef76532f09a3acf0fc4ae1b6d7ff5e3535adbc355e547b866b480a86dc501251d1117f4e092efb5b96547e45d86be0929fdbaa14bc9dbec0e047626e9cb461167c41855308ad587e98012647caf8d39024a197b5c4ef944ed662d0b8651561b46a4c606b0d486ce593b102ad3bc09d9aebdfeef7f233da9ae3c641ef533ff1ed088938c51f40ad99206e471e75626348a3012418faa705838656875529337eb1e17b760230188a7cc7b5c640d8683f3e3bb8854a9c13be48335baaebb36fbf07fbdc145d07f4651570d70abc3fec83ec0fdb72339b1e7fde1ccffb6c5462479ca156df921ab63da2e5a27ebb2496043a9938f59a20531493425ee81ac57529fb0b8a0ad326b03b6b1a9488144f77191344c64b1d4b0f5d45a40c3d0e07642862b4b449350d9060f4fb0a7329a26924f5d57725c4f4cc8a2fcc9e15aabf08c066626458c81a7ee3cb8116667a4481690a54b7b071be1f81dd09e4505a1a7c16632e76271b146c507d9ebe0a3df8ea23ef8111f693cbd77958130e67dcaf0540eb475e6a9e22041767e31596e842516e9bb2f2ba215f3d89d7eb12a45246dfbb14b5a63be100f59da022c47f348656166c561f3089ff414c1fc283f69111203493fda39262215ddb3215afce7c50cea044dc2d576d8f257f4a4db22a47c6fda69c14629c140ba59e1ffe3a97cc0728a4d2a00fb3935acc95b78fa39fcf82e565237fc1b48002146b46105725eabc55cdef8482824503d12c9d89feb0fde2c90c4b7bd946d8"
-window.snarkProof = snarkProof
-export class SunkEthInteracter {
+//const hardCodedBurnAddress = toUnspendableAddress(hardCodedSecret)
+//const hardCodedAmount = ethers.parseEther('0.0069')
 
+export class SunkEthInteracter {
+    /**
+     * 
+     * @param {ethers.HexString} input 
+     * @param {Number} bytes 
+     * @returns 
+     */
+    static splitBytes = (input, bytes) => {
+        const regEx = new RegExp(`.{1,${2*bytes}}`, "g")
+        return input.slice(2).match(regEx).map((x)=>"0x"+x)
+
+    }
     /**
      * 
      * @param {ethers.AddressLike} sunkEthContractAddress 
@@ -113,7 +127,6 @@ export class SunkEthInteracter {
 
     static makeValuesIntoNumbers(obj) {
         return Object.fromEntries(Object.entries(obj).map((x)=>{
-            console.log(x[1])
             if (typeof(x[1]) === "string" && x[1].startsWith("0x")) {
                 //return [x[0], BigInt(x[1]).toString()]
 
@@ -131,151 +144,138 @@ export class SunkEthInteracter {
         }))
     }
 
-    async unShieldTokens(nullifierData) {
-        //const storageProof = await getStateProofs(burnAddress, this.provider)
-        
-        //console.log(blockHeaderRLP)
-        //console.log("real blockhash: ", block.hash)
-        //console.log("rlp reconstructec blockhash: ", ethers.keccak256(ethers.toBeArray(blockHeaderRLP)))
-
-
-
-        //const SECRET_KEY = ethers.hexlify(nullifierData.secret) //'0xbc005f65414869c630b7ae67836847f46f9083a9cd84af3f01969031309e5c1d'
-        //const BLOCK_NUMBER = (await this.provider.getBlock()).number
-        // const CONTRACT_ADDRESS =   this.sunkEthContract.target //'0x46CFe55bf2E5A02B738f5BBdc1bDEE9Dd22b5d39'
-        // const UNSPENDABLE_ADDRESS = toUnspendableAddress(SECRET_KEY)
-        // //const BALANCE_TO_BURN = parseEther('0.069420')
-        // const STORAGE_KEY = getStorageKey(0n, UNSPENDABLE_ADDRESS)
-
-
-        // const unspendable_address = nullifierData.burnAddressBytes;
-        // const pubNullifier = nullifierData.nullifierBytes; 
-        // const s = nullifierData.secret;
-        // const input = {s:Array.from(s),unspendable_address:Array.from(unspendable_address),pubNullifier:Array.from(pubNullifier)}
-        // console.log(input)
-        // console.log("circuit: ", circuit)
-        // const proof = await noir.generateProof(input);
-        // console.log("proof: ", proof)
-        // console.log ("is verified (local): ",await noir.verifyProof(proof))
-        // console.log(nullifierData)
-        // console.warn("TODO implement")
-
-        // // uint256 wad,
-        // // bytes32 nullifier,
-        // // bytes calldata blockHeaderRLP,
-        // // bytes calldata snarkProof
-        // const amount = await this.sunkEthContract.balanceOf(burnAddress)
-        // console.log("args: ", {amount, pubNullifier, blockHeaderRLP, proof:proof.proof})
-        // //console.log("args arr: ", JSON.stringify(amount, pubNullifier, blockHeaderRLP, proof.proof))
-        // console.log(amount, ethers.hexlify(pubNullifier), blockHeaderRLP,ethers.hexlify( proof.proof))
+    async unShieldTokens(SECRET_KEY, BLOCK_NUMBER, snarkProof=undefined) {
         const CONTRACT_ADDRESS =   this.sunkEthContract.target //'0x46CFe55bf2E5A02B738f5BBdc1bDEE9Dd22b5d39'
-        const SECRET_KEY = ethers.hexlify(nullifierData.secret)
+
         const UNSPENDABLE_ADDRESS = toUnspendableAddress(SECRET_KEY)
         const STORAGE_KEY = getStorageKey(0n, UNSPENDABLE_ADDRESS)
-        
-        const nullifier = ethers.sha256(ethers.concat([SECRET_KEY, ethers.sha256(SECRET_KEY)]))
 
-        const BLOCK_NUMBER = (await this.provider.getBlock()).number
+        const nullifier = ethers.sha256(ethers.concat([SECRET_KEY, ethers.sha256(SECRET_KEY)]))
+        
+
         const block = await this.provider.send('eth_getBlockByNumber', [
             `0x${BLOCK_NUMBER.toString(16)}`,
             false,
         ])
-        const blockHeaderRLP = await getBlockWithRLP(block.hash, this.provider)
 
-        const proof = await this.provider.send('eth_getProof', [
+        const storageProof = await this.provider.send('eth_getProof', [
             CONTRACT_ADDRESS,
             [STORAGE_KEY],
             block.number,
         ])
 
-        const accountStateRlp = ethers.decodeRlp(proof.accountProof.slice(-1)[0])
+        const accountStateRlp = ethers.decodeRlp(storageProof.accountProof.slice(-1)[0])
         const accountStateValue = ethers.getBytes(accountStateRlp[1]) // NB: also RLP-encoded
 
+
         const processedStateProof = processProof(
-            proof.accountProof,
-            proof.address,
+            storageProof.accountProof,
+            storageProof.address,
             accountStateValue,
             MAX_DEPTH,
             MAX_TRIE_NODE_LENGTH,
             MAX_ACCOUNT_STATE_LENGTH,
         )
 
+        console.log(processedStateProof)
+        processedStateProof.storageHash = storageProof.storageHash
+        const storageRootOffset = SunkEthInteracter.getStorageRootOffset(processedStateProof)
+
         const processedStorageProof = processProof(
-            proof.storageProof[0].proof,
-            proof.storageProof[0].key,
-            proof.storageProof[0].value,
+            storageProof.storageProof[0].proof,
+            storageProof.storageProof[0].key,
+            storageProof.storageProof[0].value,
             MAX_DEPTH,
             MAX_TRIE_NODE_LENGTH,
             MAX_STORAGE_VALUE_LENGTH,
         )
-        let storageRootOffset = ethers.hexlify(new Uint8Array(processedStateProof.value)).lastIndexOf(
+
+       
+
+        const formattedStateProof = {
+            key: ethers.hexlify(new Uint8Array(processedStateProof.key)),
+            proof: ethers.hexlify(new Uint8Array(processedStateProof.proof)),
+            depth: processedStateProof.depth,
+            value: ethers.hexlify(new Uint8Array(processedStateProof.value)),
+        }
+
+
+        const formattedStorageProof = {
+            key: ethers.hexlify(new Uint8Array(processedStorageProof.key)),
+            proof: ethers.hexlify(new Uint8Array(processedStorageProof.proof)),
+            depth: processedStorageProof.depth,
+            value: ethers.hexlify(new Uint8Array(processedStorageProof.value)),
+        }
+
+        const blockHeaderRLP = await getBlockWithRLP(block.hash, this.provider)
+    
+
+        const args = {
+            balance: ethers.zeroPadValue(ethers.hexlify(new Uint8Array(processedStorageProof.value)), 32),
+            nullifier: nullifier,
+            storageRootOffset: storageRootOffset,
+            stateProof: formattedStateProof,
+            blockHeaderRLP: blockHeaderRLP,
+        }
+
+
+        const split = (input)=>SunkEthInteracter.splitBytes(input, 1)
+
+
+        if (snarkProof === undefined) {
+            const snarkInput = {
+                s: split(SECRET_KEY), 
+                balance: split(args.balance),
+                state_root: split(block.stateRoot),
+                state_proof: {
+                    key: split(formattedStateProof.key),
+                    proof: split(formattedStateProof.proof),
+                    depth: formattedStateProof.depth,
+                    value: split(formattedStateProof.value)
+                },
+                storage_root:   split(storageProof.storageHash), 
+                storage_proof: {
+                    key: split(formattedStorageProof.key),
+                    proof: split(formattedStorageProof.proof),
+                    depth: formattedStorageProof.depth,
+                    value: split(formattedStorageProof.value)
+                }
+            }
+            console.log("snark input: ",snarkInput)
+            await SunkEthInteracter.setupWasm()
+            const backend = new BarretenbergBackend(circuit);
+            const noir = new Noir(circuit, backend);
+           
+            snarkProof = await noir.generateProof(snarkInput);
+        }
+
+        args.snarkProof = snarkProof
+
+
+
+        console.log(args)
+        const tx = await this.sunkEthContractWithSigner.remint(
+            args.balance,
+            args.nullifier,
+            args.storageRootOffset,
+            args.stateProof,
+            args.blockHeaderRLP,
+            args.snarkProof,
+        )
+        return tx
+    }
+
+    static getStorageRootOffset(proof) {
+        console.log("PROOF ", proof)
+        let storageRootOffset = ethers.hexlify(new Uint8Array(proof.value)).lastIndexOf(
             proof.storageHash.slice(2),
         ) // dodgy af
+        console.log(storageRootOffset)
         if (storageRootOffset === -1) {
             throw new Error('Could not find storageRoot in value')
         }
         storageRootOffset = (storageRootOffset - 2) / 2
-
-        const remintCalldata = {
-            balance: ethers.zeroPadValue(ethers.hexlify(new Uint8Array(processedStorageProof.value)), 32),
-            storageRootOffset,
-            stateProof: {
-                key: ethers.hexlify(new Uint8Array(processedStateProof.key)),
-                proof: ethers.hexlify(new Uint8Array(processedStateProof.proof)),
-                depth: processedStateProof.depth,
-                value: ethers.hexlify(new Uint8Array(processedStateProof.value)),
-            },
-            storageRoot: proof.storageHash,
-        }
-
-        await SunkEthInteracter.setupWasm()
-        const backend = new BarretenbergBackend(circuit);
-        const noir = new Noir(circuit, backend);
-
-        const inputPlain = {
-            s:(SECRET_KEY), 
-            balance:(remintCalldata.balance), 
-            state_root: (block.stateRoot),
-            state_proof: (remintCalldata.stateProof),
-            storage_root: (remintCalldata.storageRoot), 
-            storage_proof: (remintCalldata.stateProof)
-            
-
-        }
-        console.log(inputPlain)
-
-        
-        // const input = {
-        //     s: BigInt(SECRET_KEY).toString(), 
-        //     balance: BigInt(remintCalldata.balance).toString(), 
-        //     state_root: BigInt(block.stateRoot).toString(),
-        //     state_proof: SunkEthInteracter.makeValuesIntoNumbers(remintCalldata.stateProof),
-        //     storage_root:   BigInt(remintCalldata.storageRoot).toString(), 
-        //     storage_proof:  SunkEthInteracter.makeValuesIntoNumbers(remintCalldata.stateProof)
-            
-
-        // }
-        // console.log(input)
-        //const snarkProof = await noir.generateProof(input);
-        //const snarkProof = JSON.stringify(document.getElementById("proof").value)
-        console.log({
-            balance:remintCalldata.balance,
-            nullifier,
-            storageRootOffset: remintCalldata.storageRootOffset,
-            stateProof: remintCalldata.storageRootOffset,
-            stateProof: remintCalldata.stateProof,
-            snarkProof: snarkProof.toString()
-        })
-        const tx = await this.sunkEthContractWithSigner.remint(
-            remintCalldata.balance,
-            nullifier,
-            remintCalldata.storageRootOffset,
-            remintCalldata.stateProof,
-            blockHeaderRLP,
-            ethers.toBeArray(snarkProof),
-        )
-        //const tx = await this.sunkEthContractWithSigner.remint(amount, ethers.hexlify(pubNullifier), blockHeaderRLP,ethers.hexlify( proof.proof))
-        return tx
+        return storageRootOffset
     }
 
     async isClaimed(nullifier) {
@@ -334,4 +334,3 @@ export class SunkEthInteracter {
 }
 
 const secret = new Uint8Array([223, 2, 3, 64, 160, 235, 45, 12, 98, 202, 111, 134, 57, 86, 76, 30, 208, 94, 40, 196, 212, 31, 56, 207, 174, 228, 240, 24, 98, 250, 227, 179])
-console.log(SunkEthInteracter.getNullifierData(secret))
